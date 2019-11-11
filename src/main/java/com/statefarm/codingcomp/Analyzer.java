1a506fec05054c9bf587619bae2d53dd19c5c3e8
Harshal Sheth <hsheth2@gmail.com>
2019-11-11 08:53:44 -0700

    Fix bug in provided code

--- src/main/java/com/statefarm/codingcomp/Analyzer.java
+++ src/main/java/com/statefarm/codingcomp/Analyzer.java
@@ -9,6 +9,9 @@ public class Analyzer {
 
 	public static void main(String[] args) throws Exception {
 		List<Policy> policies = new Reader().read();
+		assert policies.size() == 10000;
 		//TODO - find something interesting in the data
+
+        policies.stream().forEach((policy -> System.out.println(policy.getAge())));
 	}
 }
